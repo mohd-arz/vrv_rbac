b4e03d1a6cfe17cb0fa269cad1770929d7d982eb
Mohammed Arsh M <mohammedarsh75@gmail.com>
2024-11-29 09:53:34 +0530

    Update index.ts
    cors local specified removed

--- backend/index.ts
+++ backend/index.ts
@@ -16,10 +16,7 @@ const app: Express = express();
 
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.urlencoded({ extended: true }));
